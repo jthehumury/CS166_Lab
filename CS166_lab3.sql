a0223cab74323d201331d0bb8b846b1ef2234d83
otniel Thehumury <otheh001@wch135-24.cs.ucr.edu>
2021-04-23 21:26:58 -0700

    Added more tables

--- CS166_lab3.sql
+++ CS166_lab3.sql
@@ -1,6 +1,27 @@
-DROP TABLE IF EXISTS Professors;
-CREATE TABLE Professors (SSN text,Name text,Age INTEGER,Rank text,Specialty text);
-CREATE TABLE Projects (PNO INTEGER,Sponsor text,Start_data text,End_data text,Budget REAL);
-CREATE TABLE Departments (DNO INTEGER,Dname text,Office text);
-CREATE TABLE Graduate (SSN text,Name text,Age INTEGER,Deg_pg text);
+DROP TABLE IF EXISTS Professor;
+CREATE TABLE Professor (ssn text, name text, age INTEGER, rank text, specialty text);
+DROP TABLE IF EXISTS Project;
+CREATE TABLE Project (pno INTEGER, sponsor text, start_date text, end_date text, budget REAL);
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department (dno INTEGER, dname text, office text);
+DROP TABLE IF EXISTS Graduate;
+CREATE TABLE Graduate (ssn text, name text, age INTEGER, deg_pg text);
+DROP TABLE IF EXISTS Work_in;
+CREATE TABLE Work_in (ssn text, pno text);
+DROP TABLE IF EXISTS Project_mgr;
+CREATE TABLE Project_mgr (pno INTEGER, ssn text, sponsor text, start_date text, end_date text, budget REAL);
+DROP TABLE IF EXISTS Work_proj;
+CREATE TABLE Work_proj (pno INTEGER, ssn text, since text);
+DROP TABLE IF EXISTS Works_dept;
+CREATE TABLE Works_dept (ssn DNO, pno INTEGER, since text);
+DROP TABLE IF EXISTS Dep_chair;
+CREATE TABLE Dep_chair (ddn text,  dno INTEGER, pno INTEGER, since text);
+
+DROP TABLE IF EXISTS Project_supervisor;
+CREATE TABLE Project_supervisor (ssn text, dno INTEGER, pno INTEGER, since text);
+
+DROP TABLE IF EXISTS Graduate_dept;
+CREATE TABLE Graduate_dpt (ssn text, dno INTEGER, ssn text, name text, age INTEGER, deg_pg text);
+DROP TABLE IF EXISTS Advises;
+CREATE TABLE Advises (ssn text, ssn text, name text, age INTEGER, deg_pg text);
 

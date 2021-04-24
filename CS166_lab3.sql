47056c5542f30bb2cfb7dcf06e228f0bb79c2604
otniel Thehumury <otheh001@wch135-24.cs.ucr.edu>
2021-04-23 18:43:52 -0700

    Added entity tables

--- CS166_lab3.sql
+++ CS166_lab3.sql
@@ -1,1 +1,6 @@
+DROP TABLE IF EXISTS Professors;
+CREATE TABLE Professors (SSN text,Name text,Age INTEGER,Rank text,Specialty text);
+CREATE TABLE Projects (PNO INTEGER,Sponsor text,Start_data text,End_data text,Budget REAL);
+CREATE TABLE Departments (DNO INTEGER,Dname text,Office text);
+CREATE TABLE Graduate (SSN text,Name text,Age INTEGER,Deg_pg text);
 

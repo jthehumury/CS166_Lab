40b50b1d93b775d5094a382c040e33ecd772197c
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-02 20:38:09 -0700

    Update CS166_lab4.sql

--- CS166_lab4.sql
+++ CS166_lab4.sql
@@ -1,133 +1,46 @@
-DROP TABLE IF EXISTS Professor, Project, Dept, Graduate, Work_in, Work_proj, Work_dept, Supervise;
-CREATE TABLE Professor (
-ssn text NOT NULL,
-name text,
-age INTEGER,
-rank text,
-specialty text,
-PRIMARY KEY (ssn)
+DROP TABLE IF EXISTS Books, Students, Authors, Descriptions, Borrows, Has-written, Describes;
+CREATE TABLE Books (
+DocId numeric(0,9) NOT NULL,
+Title text,
+Publisher text,
+Year numeric(0,4),
+PRIMARY KEY (DocId)
 );
-CREATE TABLE Project (
-pno INTEGER NOT NULL,
-ssn text NOT NULL,
-sponsor text,
-start_date text,
-end_date text,
-budget REAL,
-PRIMARY KEY (pno),
-FOREIGN KEY (ssn) REFERENCES Professor (ssn)
+CREATE TABLE Students (
+StId numeric(0,9) NOT NULL,
+StName text,
+Major text,
+Age numeric(0,3),
+PRIMARY KEY (StId),
 );
-CREATE TABLE Dept (
-dno INTEGER NOT NULL,
-ssn text NOT NULL,
-dname text,
-office text,
-PRIMARY KEY (dno),
-FOREIGN KEY (ssn) REFERENCES Professor (ssn)
+CREATE TABLE Authors (
+AName text NOT NULL,
+Address text,
+PRIMARY KEY (AName),
 );
-CREATE TABLE Graduate (
-ssn text NOT NULL,
-ssn_advisor text NOT NULL,
-dno INTEGER NOT NULL,
-name text,
-age INTEGER,
-deg_pg text,
-PRIMARY KEY (ssn),
-FOREIGN KEY (dno) REFERENCES Dept (dno)
+CREATE TABLE Descriptions (
+Keyword text NOT NULL,
+PRIMARY KEY (Keyword)
 );
-CREATE TABLE Work_in (
-ssn text NOT NULL,
-pno INTEGER,
-PRIMARY KEY (ssn, pno),
-FOREIGN KEY (ssn) REFERENCES Professor (ssn),
-FOREIGN KEY (pno) REFERENCES Project (pno)
+CREATE TABLE Borrows (
+DocId numeric(0,9) NOT NULL,
+StId numeric(0,9) NOT NULL,
+Date text,
+PRIMARY KEY (DocId, StId),
+FOREIGN KEY (DocId) REFERENCES Books (DocId),
+FOREIGN KEY (StId) REFERENCES Students (StId)
 );
-CREATE TABLE Work_proj (
-pno INTEGER NOT NULL,
-ssn text NOT NULL,
-since text,
-PRIMARY KEY (pno, ssn),
-FOREIGN KEY (pno) REFERENCES Project (pno),
-FOREIGN KEY (ssn) REFERENCES Graduate (ssn)
+CREATE TABLE Has-written (
+DocId numeric(0,9) NOT NULL,
+AName text NOT NULL,
+PRIMARY KEY (DocId, AName),
+FOREIGN KEY (DocId) REFERENCES Books (DocId),
+FOREIGN KEY (AName) REFERENCES Authors (AName)
 );
-CREATE TABLE Work_dept (
-ssn text,
-dno INTEGER NOT NULL,
-time_pc text,
+CREATE TABLE Describes (
+DocId numeric(0,9) NOT NULL,
+Keyword text NOT NULL,
 PRIMARY KEY (ssn, dno),
-FOREIGN KEY (ssn) REFERENCES Professor (ssn),
-FOREIGN KEY (dno) REFERENCES Dept (dno)
-);
-CREATE TABLE Supervise (
-pno INTEGER,
-ssn_graduate text,
-ssn text NOT NULL,
-since text,
-PRIMARY KEY (pno, ssn_graduate, ssn),
-FOREIGN KEY (pno) REFERENCES Project (pno),
-FOREIGN KEY (ssn_graduate) REFERENCES Graduate (ssn),
-FOREIGN KEY (ssn) REFERENCES Professor (ssn)
-);
-
-DROP TABLE IF EXISTS Musicians, Album, Songs, Instrument, Place, Telephone, Plays, Perform, Lives;
-CREATE TABLE Musicians (
-ssn text NOT NULL,
-name text,
-PRIMARY KEY (ssn)
-);
-CREATE TABLE Album (
-albumIdentifier text NOT NULL,
-copywriteDate text,
-speed text,
-title text,
-ssn text NOT NULL,
-PRIMARY KEY (albumIdentifier),
-FOREIGN KEY (ssn) REFERENCES Musicians (ssn)
-);
-CREATE TABLE Songs (
-songId text NOT NULL,
-title text,
-author text,
-albumidentifier text NOT NULL,
-PRIMARY KEY (songId),
-FOREIGN KEY (albumIdentifier) REFERENCES Album (albumIdentifier)
-);
-CREATE TABLE Instrument (
-instrId text NOT NULL,
-dname text,
-key text,
-PRIMARY KEY (instrId)
-);
-CREATE TABLE Place (
-address text NOT NULL,
-PRIMARY KEY (address)
-);
-CREATE TABLE Telephone (
-phone_no text,
-address text NOT NULL,
-PRIMARY KEY (phone_no),
-FOREIGN KEY (address) REFERENCES Place (address)
-);
-CREATE TABLE Plays (
-ssn text NOT NULL,
-instrId text NOT NULL,
-PRIMARY KEY (ssn, instrId),
-FOREIGN KEY (ssn) REFERENCES Musicians (ssn),
-FOREIGN KEY (instrId) REFERENCES Instrument (instrId)
-);
-CREATE TABLE Perform (
-ssn text NOT NULL,
-songId text NOT NULL,
-PRIMARY KEY (ssn, songId),
-FOREIGN KEY (ssn) REFERENCES Musicians (ssn),
-FOREIGN KEY (songId) REFERENCES Songs (songId)
-);
-CREATE TABLE Lives (
-address text NOT NULL,
-phone_no text NOT NULL,
-ssn text NOT NULL,
-PRIMARY KEY (address, phone_no, ssn),
-FOREIGN KEY (address) REFERENCES Place (address),
-FOREIGN KEY (phone_no) REFERENCES Telephone (phone_no),
-FOREIGN KEY (ssn) REFERENCES Musicians (ssn)
+FOREIGN KEY (DocId) REFERENCES Books (DocId),
+FOREIGN KEY (Keyword) REFERENCES Descriptions (Keyword)
 );

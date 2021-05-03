9c54d5f8ceb307f7bb0f49d4555b1a80b6ef41b3
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-02 23:33:10 -0700

    Update CS166_lab4.sql

--- CS166_lab4.sql
+++ CS166_lab4.sql
@@ -73,5 +73,5 @@ ALTER TABLE Authors RENAME COLUMN AName TO Name;
 SELECT StName FROM Students NATURAL JOIN Borrows WHERE Major = 'CS';
 SELECT Title FROM Authors NATURAL JOIN Has_written NATURAL JOIN Books WHERE Name = 'Jones';
 SELECT Title FROM Descriptions NATURAL JOIN Authors NATURAL JOIN Has_written NATURAL JOIN Books WHERE Name = 'Jones' and NOT(Keyword = 'database');
-SELECT StName FROM (SELECT StName, Age FROM Students) MINUS (SELECT d.StName, d.Age FROM (((CREATE TABLE d SELECT * FROM Students) CROSS JOIN Students) WHERE Students.Age < d.Age));
-SELECT Title FROM (SELECT Title, Year FROM Books) MINUS (SELECT d.Title, d.Year FROM (((CREATE TABLE d SELECT * FROM Books) CROSS JOIN Books) WHERE Books.Year < d.Year));
+SELECT StName FROM ((SELECT StName, Age FROM Students) MINUS (SELECT d.StName, d.Age FROM (((CREATE TABLE d SELECT * FROM Students) CROSS JOIN Students) WHERE Students.Age < d.Age)));
+SELECT Title FROM ((SELECT Title, Year FROM Books) MINUS (SELECT d.Title, d.Year FROM (((CREATE TABLE d SELECT * FROM Books) CROSS JOIN Books) WHERE Books.Year < d.Year)));

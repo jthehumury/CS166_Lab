14422ad8f02ad4f08b3951877522311254ad8c3a
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-03 00:21:43 -0700

    Update CS166_lab4.sql

--- CS166_lab4.sql
+++ CS166_lab4.sql
@@ -45,25 +45,6 @@ FOREIGN KEY (DocId) REFERENCES Books (DocId),
 FOREIGN KEY (Keyword) REFERENCES Descriptions (Keyword)
 );
 
-INSERT INTO Students VALUES (860507011,'John 1','CS',4);
-INSERT INTO Students VALUES (860507021,'John 2','CS',40);
-INSERT INTO Students VALUES (860507031,'John 3','BIO',4);
-INSERT INTO Students VALUES (860507041,'John 4','BIO',35);
-INSERT INTO Books VALUES (860507043,'b1','McGraw-Hill',1980);
-INSERT INTO Books VALUES (860507044,'b2','McGraw-Hill',2000);
-INSERT INTO Books VALUES (860507011,'b3','McGraw',1980);
-INSERT INTO Books VALUES (860507021,'b4','McGraw',2000);
-INSERT INTO Books VALUES (860507034,'b5','McGraw-Hill',2000);
-INSERT INTO Books VALUES (860507041,'b6','McGraw',1980);
-INSERT INTO Books VALUES (870507041,'b7','McGraw',2000);
-INSERT INTO Authors VALUES ('Jones','Davis');
-INSERT INTO Authors VALUES ('Jones','Davis 1');
-INSERT INTO Authors VALUES ('Jones 1','Davis');
-INSERT INTO Authors VALUES ('Jones 1','Davis 1');
-INSERT INTO Has_written VALUES (860507043,'Jones');
-INSERT INTO Has_written VALUES (870507041,'Jones1');
-INSERT INTO Describes VALUES (860507043,'database');
-
 SELECT Year, Title FROM Books;
 SELECT * FROM Students WHERE Major = 'CS';
 SELECT * FROM Students CROSS JOIN Books;
@@ -73,6 +54,6 @@ SELECT StName FROM Students WHERE NOT(Major='CS' OR Age<30);
 ALTER TABLE Authors RENAME COLUMN AName TO Name;
 SELECT StName FROM Students NATURAL JOIN Borrows WHERE Major = 'CS';
 SELECT Title FROM Authors NATURAL JOIN Has_written NATURAL JOIN Books WHERE Name = 'Jones';
-SELECT Title FROM (Descriptions NATURAL JOIN Authors NATURAL JOIN Has_written NATURAL JOIN Books) WHERE Name = 'Jones';
+SELECT Title FROM Descriptions NATURAL JOIN Authors NATURAL JOIN Has_written NATURAL JOIN Books WHERE Name = 'Jones' AND NOT(Keyword = 'database');
 SELECT StName FROM ((SELECT StName, Age FROM Students) MINUS (SELECT d.StName, d.Age FROM (((SELECT * INTO d FROM Students) WHERE Students.Age < d.Age)));
 SELECT Title FROM ((SELECT Title, Year FROM Books) MINUS (SELECT d.Title, d.Year FROM (((SELECT * INTO d FROM Books) CROSS JOIN Books) WHERE Books.Year < d.Year)));

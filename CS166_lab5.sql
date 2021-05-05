ddd09e412cdbf87f46f974dac1059fcf35a49947
otniel Thehumury <otheh001@wch135-24.cs.ucr.edu>
2021-05-05 10:05:24 -0700

    Completed lab 5

--- CS166_lab5.sql
+++ CS166_lab5.sql
@@ -1,1 +1,5 @@
-SELECT 
+SELECT pid FROM Catalog WHERE cost < 10;
+SELECT pname FROM (catalog NATURAL JOIN parts) WHERE cost < 10;
+SELECT address FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers) WHERE pname = 'Fire Hydrant Cap';
+SELECT sname FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers) WHERE color = 'Green';
+SELECT sname, pname FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers);

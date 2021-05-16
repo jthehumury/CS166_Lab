920bcb44495cffeec730c1dfcfaf172389f03b47
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-16 13:12:21 -0700

    Update CS166_lab6.sql

--- CS166_lab6.sql
+++ CS166_lab6.sql
@@ -1,7 +1,19 @@
-SELECT pid, COUNT(pid) FROM catalog GROUP BY sid;
-SELECT pid, COUNT(pid) FROM catalog WHERE COUNT(pid) > 2 GROUP BY sid;
-SELECT sname, COUNT(pid) FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers) WHERE color = ALL
-  (SELECT 
-    
-    );
-SELECT sname FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers) WHERE color = 'Green';
+SELECT sid, COUNT(pid)
+FROM catalog
+GROUP BY sid;
+
+SELECT sid, COUNT(pid)
+FROM catalog
+WHERE COUNT(pid) > 2
+GROUP BY sid;
+
+SELECT sname, COUNT(pid)
+FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers)
+WHERE color = ALL ('Green')
+GROUP BY sid;
+
+SELECT sname
+FROM ((parts NATURAL JOIN catalog) NATURAL JOIN suppliers)
+WHERE color = ANY ('Red')
+AND color = ANY ('Green')
+GROUP BY sid;

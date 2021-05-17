fab05b2d0ce5aeaf2aaaab36ddb40f21124a2548
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-17 16:16:47 -0700

    Update CS166_lab6.sql

--- CS166_lab6.sql
+++ CS166_lab6.sql
@@ -11,8 +11,9 @@ FROM catalog
 GROUP BY sid
 HAVING COUNT(pid) >= 3;
 
-SELECT sname, COUNT(*)
-FROM
+SELECT sname, COUNT(pid)
+FROM supplier NATURAL JOIN catalog
+WHERE sname =
 	(SELECT sname
 	FROM parts p1, catalog c1, suppliers s1
 	WHERE p1.pid = c1.pid
@@ -24,20 +25,24 @@ FROM
 	FROM parts p2, catalog c2, suppliers s2
 	WHERE p2.pid = c2.pid
 	AND s2.sid = c2.sid
-	AND p2.color <> 'Green') as x
+	AND p2.color <> 'Green')
 GROUP BY sname;
 
-SELECT sname
-FROM parts p1, catalog c1, suppliers s1
-WHERE p1.pid = c1.pid
-AND s1.sid = c1.sid
-AND p1.color = 'Green'
+SELECT sname, MAX(cost)
+FROM supplier NATURAL JOIN catalog
+WHERE sname =
+	(SELECT sname
+	FROM parts p1, catalog c1, suppliers s1
+	WHERE p1.pid = c1.pid
+	AND s1.sid = c1.sid
+	AND p1.color = 'Green'
+	GROUP BY sname
+
+	INTERSECT
+
+	SELECT sname
+	FROM parts p2, catalog c2, suppliers s2
+	WHERE p2.pid = c2.pid
+	AND s2.sid = c2.sid
+	AND p2.color = 'Red')
 GROUP BY sname
-
-INTERSECT
-
-SELECT sname
-FROM parts p2, catalog c2, suppliers s2
-WHERE p2.pid = c2.pid
-AND s2.sid = c2.sid
-AND p2.color = 'Red';

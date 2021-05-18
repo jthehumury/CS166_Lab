64e685814f8227a4bdfd5a99d8fdd713a12e1bb2
otniel Thehumury <otheh001@wch135-24.cs.ucr.edu>
2021-05-17 20:53:09 -0700

    Almost done pt2

--- EmbeddedSQL.java
+++ EmbeddedSQL.java
@@ -272,9 +272,9 @@ public class EmbeddedSQL {
 
    public static void Query3(EmbeddedSQL esql){
       try{
-         String query = "SELECT sname, COUNT(pid) FROM supplier NATURAL JOIN catalog WHERE sname IN\n";
+         String query = "SELECT sname, COUNT(pid) FROM suppliers NATURAL JOIN catalog WHERE sname IN\n";
 	query += "(SELECT sname FROM parts p1, catalog c1, suppliers s1 WHERE p1.pid = c1.pid AND s1.sid = c1.sid\n";
-	query += "EXCEPT\n"
+	query += "EXCEPT\n";
 	query += "SELECT sname FROM parts p2, catalog c2, suppliers s2 WHERE p2.pid = c2.pid AND s2.sid = c2.sid AND p2.color <> \'Green\'\n)";
 	query += "GROUP BY sname";
 
@@ -287,7 +287,7 @@ public class EmbeddedSQL {
 
    public static void Query4(EmbeddedSQL esql){
       try{
-         String query = "SELECT sname, MAX(cost) FROM supplier NATURAL JOIN catalog WHERE sname IN\n";
+         String query = "SELECT sname, MAX(cost) FROM suppliers NATURAL JOIN catalog WHERE sname IN\n";
 	query += "(SELECT sname FROM parts p1, catalog c1, suppliers s1 WHERE p1.pid = c1.pid AND s1.sid = c1.sid AND p1.color = \'Green\'\n";
 	query += "INTERSECT\n";
 	query += "SELECT sname FROM parts p2, catalog c2, suppliers s2 WHERE p2.pid = c2.pid AND s2.sid = c2.sid AND p2.color = \'Red\'\n)";

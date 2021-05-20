d210cfe7f815cfa4d5941d1848b3ca5f4169663f
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-19 20:10:42 -0700

    Update CS166_Project_Phase_2.sql

--- CS166_Project_Phase_2.sql
+++ CS166_Project_Phase_2.sql
@@ -11,15 +11,15 @@ CREATE TABLE Appointment (appnt_ID CHAR(10) NOT NULL, date CHAR(30), time_slot C
 CREATE TABLE Department_includes (dept_ID CHAR(10) NOT NULL, hospital_ID CHAR(10) NOT NULL, name CHAR(30),
 	UNIQUE(dept_ID),
 	PRIMARY KEY(hospital_ID, dept_ID),
-	FOREIGN KEY(hospital_ID) REFERENCES Hospital(hospital_ID));
+	FOREIGN KEY(hospital_ID) REFERENCES Hospital(hospital_ID) ON DELETE NO ACTION);
 CREATE TABLE Doctor_worksDept (doctor_ID CHAR(10) NOT NULL, dept_ID CHAR(10) NOT NULL, name CHAR(30), specialty CHAR(30),
 	UNIQUE(doctor_ID),
 	PRIMARY KEY(doctor_ID, dept_ID),
-	FOREIGN KEY(dept_ID) REFERENCES Department_includes(dept_ID));
+	FOREIGN KEY(dept_ID) REFERENCES Department_includes(dept_ID) ON DELETE NO ACTION);
 CREATE TABLE Staff_worksIn (staff_ID CHAR(10) NOT NULL, name CHAR(30), hospital_ID CHAR(10) NOT NULL,
 	UNIQUE(staff_ID),
 	PRIMARY KEY(hospital_ID, staff_ID),
-	FOREIGN KEY(hospital_ID) REFERENCES Hospital(hospital_ID));
+	FOREIGN KEY(hospital_ID) REFERENCES Hospital(hospital_ID) ON DELETE NO ACTION);
 
 
 /*Weak Entities: past, active, available, waitlisted*/

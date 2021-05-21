cc2962d165f6b68b56fc82949d521e723f3d18ee
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-21 15:08:35 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,13 @@
+SELECT * FROM part_nyc
+SELECT * FROM part_sfo
+SELECT * FROM supplier
+SELECT * FROM color
+
+SELECT COUNT(*)
+FROM part_nyc
+WHERE on_hand > 70;
+
+SELECT COUNT(*)
+FROM part_nyc, 
+WHERE color = 'Red'
 

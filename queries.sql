80ad3b51ef705134de1a21c67ad98ddb2b25ef51
jthehumury <62961426+jthehumury@users.noreply.github.com>
2021-05-21 20:38:35 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,7 +7,27 @@ SELECT COUNT(*)
 FROM part_nyc
 WHERE on_hand > 70;
 
-SELECT COUNT(*)
-FROM part_nyc, 
-WHERE color = 'Red'
 
+SELECT COUNT(on_hand)
+FROM 
+  (part_nyc
+   UNION
+   part_sfo)
+WHERE color = 'Red';
+
+
+SELECT supplier, COUNT(on_hand)
+FROM part_nyc
+
+SELECT supplier, COUNT(on_hand)
+FROM part_sfo
+
+SELECT supplier
+FROM 
+  (SELECT part_number, supplier
+  FROM part_nyc
+   
+  EXCEPT
+  
+  SELECT part_number, supplier
+  part_sfo S);
